bc416840fb3fcc5fb11a2de85d58cc3ab40a56e0
Irvin Cote <irvincoteg@gmail.com>
2024-04-26 14:27:51 +0300

    Add build script to regenerate parser code upon changes in the grammar file

---
+++ build.rs
@@ -0,0 +1,60 @@
+use std::convert::TryInto;
+use std::env;
+use std::env::VarError;
+use std::error::Error;
+use std::fs::{read_dir, DirEntry, File};
+use std::io::Write;
+use std::path::Path;
+use std::process::Command;
+
+fn main() {
+    let grammars = vec![
+        "ifcc",
+    ];
+    let additional_args = vec![
+        Some("-visitor"),
+        Some("-no-listener"),
+    ];
+    let antlr_path = "../antlr_tool/antlr4-4.8-2-SNAPSHOT-complete.jar";
+
+    for (grammar, arg) in grammars.into_iter().zip(additional_args) {
+        //ignoring error because we do not need to run anything when deploying to crates.io
+        let _ = gen_for_grammar(grammar, antlr_path, arg);
+    }
+
+    println!("cargo:rerun-if-changed=build.rs");
+
+    println!("cargo:rerun-if-changed=antlr_tool/antlr4-4.8-2-SNAPSHOT-complete.jar");
+}
+
+fn gen_for_grammar(
+    grammar_file_name: &str,
+    antlr_path: &str,
+    additional_arg: Option<&str>,
+) -> Result<(), Box<dyn Error>> {
+    // let out_dir = env::var("OUT_DIR").unwrap();
+    // let dest_path = Path::new(&out_dir);
+
+    let input = env::current_dir().unwrap().join("src");
+    let file_name = grammar_file_name.to_owned() + ".g4";
+
+    let c = Command::new("java")
+        .current_dir(input)
+        .arg("-cp")
+        .arg(antlr_path)
+        .arg("org.antlr.v4.Tool")
+        .args(additional_arg)
+        .arg("-Dlanguage=Rust")
+        .arg("-o")
+        .arg("generated")
+        .arg(&file_name)
+        .spawn()
+        .expect("antlr tool failed to start")
+        .wait_with_output()?;
+    // .unwrap()
+    // .stdout;
+    // eprintln!("xx{}",String::from_utf8(x).unwrap());
+
+    println!("cargo:rerun-if-changed=src/{}", file_name);
+    Ok(())
+}
